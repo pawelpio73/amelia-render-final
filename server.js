17ddd20257793729fa5a2c2b1df8d81caffb9329
pawelpio73 <pawelpio73@gmail.com>
2025-05-15 18:43:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const OpenAI = require("openai");
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
